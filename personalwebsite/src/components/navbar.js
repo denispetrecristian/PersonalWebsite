049b19b9caf287c30774d594ef9f26c3f308d931
Denis-Cristian Petre <denis-cristian.petre@student.manchester.ac.uk>
2021-02-05 19:21:00 +0200

    Add flex element on the navbar

--- personalwebsite/src/components/navbar.js
+++ personalwebsite/src/components/navbar.js
@@ -1,26 +1,31 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {Nav, Navbar, NavDropdown } from "react-bootstrap";
+import "./Navbar.css";
 
 function navbar() {
     return (
        <>
-        <Navbar className="navbar">
-            <div className="navbar-container">
-                <Navbar.Collapse>
-                    <Nav.Link href="#">Home</Nav.Link>
+        <Navbar className="navbar" >
+
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav>
+                    <Nav.Link href="#" className="nav-link">Home</Nav.Link>
 
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Open-Source</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Personal Projects</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">University Projects</NavDropdown.Item>
-                        
+                        <NavDropdown.Item href="#action/3.1" className="nav-link">Open-Source</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2" className="nav-link">Personal Projects</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3" className="nav-link">University Projects</NavDropdown.Item>
+                                
                     </NavDropdown>
 
-                    <Nav.Link href=""> Contact </Nav.Link>
+                    <Nav.Link href="" className="nav-link"> Contact </Nav.Link>
+                </Nav>
 
-                </Navbar.Collapse>
-            </div>
+            </Navbar.Collapse>
+               
+            
         </Navbar>
+     
        </>
     )
 }
