7e0792286f910b3d42ea4373e0a67df75014d8bf
Denis-Cristian Petre <denis-cristian.petre@student.manchester.ac.uk>
2021-07-16 12:22:25 +0300

    Initial commit

--- personalwebsite/src/components/navbar.js
+++ personalwebsite/src/components/navbar.js
@@ -5,25 +5,21 @@ import "./Navbar.css";
 function navbar() {
     return (
        <>
-        <Navbar className="navbar" >
-
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav>
-                    <Nav.Link href="#" className="nav-link">Home</Nav.Link>
-
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1" className="nav-link">Open-Source</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2" className="nav-link">Personal Projects</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3" className="nav-link">University Projects</NavDropdown.Item>
-                                
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                     </NavDropdown>
-
-                    <Nav.Link href="" className="nav-link"> Contact </Nav.Link>
                 </Nav>
-
+    
             </Navbar.Collapse>
-               
-            
         </Navbar>
      
        </>
